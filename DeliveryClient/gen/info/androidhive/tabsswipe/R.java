a8dd8cdc7e6daa6c49e0b559aaaebf3e98589457
Bachir.assaf90 <bachir.assaf90@gmail.com>
2014-05-01 13:04:50 +0000

    Create custom Adapter to work with all types

--- DeliveryClient/gen/info/androidhive/tabsswipe/R.java
+++ DeliveryClient/gen/info/androidhive/tabsswipe/R.java
@@ -27,11 +27,12 @@ public final class R {
         public static final int myred=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int keeploggedin=0x7f080006;
         public static final int languageSP=0x7f080007;
         public static final int list=0x7f080001;
         public static final int main=0x7f080002;
+        public static final int name=0x7f080009;
         public static final int pager=0x7f080000;
         public static final int password=0x7f080004;
         public static final int password2=0x7f080005;
@@ -43,6 +44,11 @@ public final class R {
         public static final int fragment_cart=0x7f030001;
         public static final int fragment_orders=0x7f030002;
         public static final int fragment_profile=0x7f030003;
+        public static final int row_add=0x7f030004;
+        public static final int row_cart=0x7f030005;
+        public static final int row_product=0x7f030006;
+        public static final int row_txt=0x7f030007;
+        public static final int row_txtimg=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;

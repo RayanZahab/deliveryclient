6f6d3606ffa383e9ffc7eacbc377f71789f5e865
Bachir.assaf90 <bachir.assaf90@gmail.com>
2014-07-08 14:33:19 +0000

    layout of add address and preview activity

--- DeliveryClient/src/info/androidhive/tabsswipe/AddAddressActivity.java
+++ DeliveryClient/src/info/androidhive/tabsswipe/AddAddressActivity.java
@@ -2,7 +2,9 @@ package info.androidhive.tabsswipe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class AddAddressActivity extends Activity {
 
@@ -10,6 +12,11 @@ public class AddAddressActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_address);
+	}
+	
+	public void submit (View v){
+		Intent i = new Intent(this, SelectAdress.class);
+		startActivity (i);
 	}
 
 	@Override

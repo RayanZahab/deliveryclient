b6b456a476e96fd3a3a970e6959b556af5b2e3fc
differentruna <differentruna@gmail.com>
2015-03-10 19:53:31 +0000

    changed data loading

--- DeliveryClient/src/info/androidhive/tabsswipe/MainActivity.java
+++ DeliveryClient/src/info/androidhive/tabsswipe/MainActivity.java
@@ -11,7 +11,6 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -48,18 +47,11 @@ public class MainActivity extends Activity {
 	private CharSequence mTitle;
 
 	// slide menu items
-	private List navMenuTitles = new ArrayList<String>();
+	private List<String> navMenuTitles = new ArrayList<String>();
 
 	public static ArrayList<NavDrawerItem> navDrawerItems;
 	private static NavDrawerListAdapter adapter;
-	public static List<Fragment> fragments = new ArrayList<Fragment>();
-
-	int i, countryP, cityP, areaP;
-	ArrayList<Country> countries = new ArrayList<Country>();
-	ArrayList<City> cities = new ArrayList<City>();
-	ArrayList<Area> areas = new ArrayList<Area>();
-	int CityId;
-	boolean last = false;
+	public static List<Fragment> fragments = new ArrayList<Fragment>();	
 
 	@SuppressLint("NewApi")
 	@Override
@@ -81,7 +73,6 @@ public class MainActivity extends Activity {
 		// enabling action bar app icon and behaving it as toggle button
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
-		countries = ((deliveryclient) this.getApplication()).getCountries();
 		Bundle extras = getIntent().getExtras();
 		int fragmentIndex = 0;
 		if(extras!=null)
@@ -297,13 +288,7 @@ public class MainActivity extends Activity {
 	public void callMethod(String m, String s, String error) {
 		Log.d("ray", "calling method: " + m);
 
-		if (m.equals("setCountries"))
-			setCountries(s, error);
-		else if (m.equals("setCities"))
-			setCities(s, error);
-		else if (m.equals("setAreas"))
-			setAreas(s, error);
-		else if (m.equals("setOrders")) {
+		if (m.equals("setOrders")) {
 			Log.d("ray", "Here5: " + m);
 			for (Fragment fragment : fragments) {
 				if (fragment.getClass().equals(PendingOrdersFragment.class)) {
@@ -447,53 +432,5 @@ public class MainActivity extends Activity {
 		return null;
 	}
 
-	public void getCountries() {
-		String serverURL = new myURL("countries", null, 0, 30).getURL();
-		RZHelper p = new RZHelper(serverURL, this, "setCountries", false);
-		p.get();
-	}
-
-	public void setCountries(String s, String error) {
-		countries = new APIManager().getCountries(s);
-		for (int j = 0; j < countries.size(); j++) {
-			getCities(j);
-		}
-	}
-
-	public void getCities(int position) {
-		countryP = position;
-		int countryId = countries.get(position).getId();
-		String serverURL = new myURL("cities", "countries", countryId, 30)
-				.getURL();
-		RZHelper p = new RZHelper(serverURL, this, "setCities", false);
-		p.get();
-	}
-
-	public void setCities(String s, String error) {
-		cities = new APIManager().getCitiesByCountry(s);
-		for (int j = 0; j < cities.size(); j++) {
-			if (j == cities.size() - 1)
-				last = true;
-			getAreas(j);
-		}
-	}
-
-	public void getAreas(int position) {
-		cityP = position;
-		CityId = cities.get(position).getId();
-		String serverURL = new myURL("areas", "cities", CityId, 30).getURL();
-		RZHelper p = new RZHelper(serverURL, this, "setAreas",false,last);
-		p.get();
-	}
-
-	public void setAreas(String s, String error) {
-		areas = new APIManager().getAreasByCity(s);
-
-		cities.get(cityP).setAreas(areas);
-		countries.get(countryP).setCities(cities);	
-		if (last) {
-			((deliveryclient) this.getApplication()).setCountries(countries);			
-			Log.d("ray", "setting countries: " + CityId + "->"+areas.size());
-		}
-	}
+	
 }

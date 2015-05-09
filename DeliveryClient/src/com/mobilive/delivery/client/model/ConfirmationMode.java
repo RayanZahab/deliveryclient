91ad2f10c447bc6bc8f8507e6a130c51142069ba
nebrassal <nebrassal@gmail.com>
2015-05-09 12:21:15 +0000

    package renaming.

--- DeliveryClient/src/com/mobilive/delivery/client/model/ConfirmationMode.java
+++
@@ -1,7 +0,0 @@
-package com.mobilive.delivery.client.model;
-
-import java.io.Serializable;
-
-public enum ConfirmationMode implements Serializable{
-	NewUser,RestPassword,ForgetPassword;
-}
--- DeliveryClient/src/com/mobilive/delivery/client/model/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.mobilive.delivery.client.model;
-
-
-public enum Gender {
-	Male,Female;
-}
